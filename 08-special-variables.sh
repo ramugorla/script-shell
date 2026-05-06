52e26c7b6a081ae3df03f301f3d431c16383b709
Gangaiah@1719 <ramugorla.brl@gmail.com>
2026-05-06 23:16:56 +0530

    hello world

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -6,5 +6,5 @@ echo "Script name:: $0"
 echo "User name who is running script:: $USER"
 echo "Present working dire:: $PWD"
 echo "Process ID of script file:: $$"
-echo sleep 10 &
+sleep 10 &
 echo "Process ID of previous command:: $!"
